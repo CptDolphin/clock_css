06fd1b7473d543613565de31c77cda0db2dd6555
CptDolphin <rafal.walas@protonmail.com>
2019-11-13 02:30:02 +0100

    refactored and fisnihed clock

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,1 +1,21 @@
-setInterval(() => {window.location.reload()}, 5000);
+setInterval(setClock, 1000);
+
+const [hourHand, minHand, secHand] = document.querySelectorAll('.hand');
+
+
+function setClock(){
+    const currDate = new Date();
+    const secRatio = currDate.getSeconds() / 60;
+    const minRatio = (secRatio + currDate.getMinutes()) / 60;
+    const hourRatio = (minRatio + currDate.getHours()) / 12;
+
+    setRotation(secHand, secRatio);
+    setRotation(minHand, minRatio);
+    setRotation(hourHand, hourRatio);
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360);
+}
+
+setClock();
